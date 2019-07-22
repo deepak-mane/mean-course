9db5b6b4fd7ae3fa18c80505ff2beb5142f48085
Deepak Mane <deepak.mane123@gmail.com>
2019-07-22 01:10:12 -0700

    39.Understanding CORS

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -25,7 +25,7 @@ export class PostListComponet implements OnInit, OnDestroy {
     constructor(public postsService: PostsService) {}
     
     ngOnInit() {
-      this.posts = this.postsService.getPosts();
+      this.postsService.getPosts();
       this.postsSub = this.postsService.getPostUpdateListener()
         .subscribe((posts: Post[]) => {
             this.posts = posts;
